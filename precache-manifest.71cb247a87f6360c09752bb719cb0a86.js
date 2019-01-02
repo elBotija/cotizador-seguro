1ed7b5d143aae065712bea92417007b2c64acd40
murillo <murillo@localhost.localdomain>
2019-01-02 20:02:08 -0300

    Updates

---
+++ precache-manifest.71cb247a87f6360c09752bb719cb0a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "405435f7c3ab854fbe6e",
+    "url": "/cotizador-seguro/static/js/runtime~main.405435f7.js"
+  },
+  {
+    "revision": "904fea24c5ce9d4cc158",
+    "url": "/cotizador-seguro/static/js/main.904fea24.chunk.js"
+  },
+  {
+    "revision": "2c4c5d307025bfb0794d",
+    "url": "/cotizador-seguro/static/js/1.2c4c5d30.chunk.js"
+  },
+  {
+    "revision": "904fea24c5ce9d4cc158",
+    "url": "/cotizador-seguro/static/css/main.cbe0cc6f.chunk.css"
+  },
+  {
+    "revision": "2c4c5d307025bfb0794d",
+    "url": "/cotizador-seguro/static/css/1.64d680e9.chunk.css"
+  },
+  {
+    "revision": "a7da320c1f89845b5e258e2aa78eb589",
+    "url": "/cotizador-seguro/index.html"
+  }
+];
